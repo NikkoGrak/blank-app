3c38c7576d56956e20cbec9d0622b089a6094505
NikkoGrak <106216132+NikkoGrak@users.noreply.github.com>
2024-11-13 23:47:22 +0700

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -3,7 +3,7 @@
 import matplotlib.pyplot as plt
 import streamlit as st
 
-def plot_route_with_satelite(best_route, waypoints, start_point, end_point):
+def plot_route_with_satelite(best_route, waypoints, start_point, end_point, titlename):
     latitudes = [start_point[0]] + [waypoints[i][0] for i in best_route] + [end_point[0]]
     longitudes = [start_point[1]] + [waypoints[i][1] for i in best_route] + [end_point[1]]
     fig, ax = plt.subplots(figsize=(10, 10))
@@ -11,7 +11,7 @@ def plot_route_with_satelite(best_route, waypoints, start_point, end_point):
     ax.plot(start_point[1], start_point[0], 'ro', markersize=10, label='Start/End Point')
     for i, (lat, lon) in enumerate(zip(latitudes, longitudes)):
         ax.annotate(f'{i}', (lon, lat))
-    ax.set_title("Best Route for TSP with Ant Colony Optimize")
+    ax.set_title("Best Route for TSP with {titlename}")
     ax.set_xlabel("Longitude")
     ax.set_ylabel("Latitude")
     ax.legend()
