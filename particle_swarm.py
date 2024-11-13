9bbad437b75ea431c7140d2833fdb9b1d5cfdeb7
NikkoGrak <106216132+NikkoGrak@users.noreply.github.com>
2024-11-14 01:23:18 +0700

    Update particle_swarm.py

--- particle_swarm.py
+++ particle_swarm.py
@@ -22,20 +22,22 @@ def total_distance(route, start_point, end_point):
 
 # Kelas PSO untuk TSP
 class PSO_TSP:
-    def __init__(self, waypoints, start_point, end_point, num_particles, num_iterations, alpha, beta):
+    def __init__(self, waypoints, start_point, end_point, num_particles, num_iterations, inertia_weight, c1, c2):
         self.waypoints = waypoints
         self.start_point = start_point
         self.end_point = end_point
         self.num_particles = num_particles
         self.num_iterations = num_iterations
-        self.alpha = alpha  # Faktor inersia
-        self.beta = beta    # Faktor pembelajaran
+        self.inertia_weight = inertia_weight  # Faktor inersia
+        self.c1 = c1  # Koefisien kognitif
+        self.c2 = c2  # Koefisien sosial
 
         self.distances = self.calculate_distances_matrix()
         self.particles = [random.sample(range(len(waypoints)), len(waypoints)) for _ in range(num_particles)]
         self.p_best = self.particles[:]
         self.g_best = min(self.particles, key=lambda p: self.route_distance(p))
-        self.velocities = [random.sample(range(len(waypoints)), len(waypoints)) for _ in range(num_particles)]
+        self.velocities = [self.initialize_velocity() for _ in range(num_particles)]
+
     def calculate_distances_matrix(self):
         num_waypoints = len(self.waypoints)
         distances = np.zeros((num_waypoints, num_waypoints))
@@ -48,30 +50,39 @@ class PSO_TSP:
     def route_distance(self, route):
         waypoints_order = [self.waypoints[i] for i in route]
         return total_distance(waypoints_order, self.start_point, self.end_point)
-    def update_particles(self):
-        for i, particle in enumerate(self.particles):
-            # Update kecepatan
-            self.velocities[i] = random.sample(range(len(self.waypoints)), len(self.waypoints))
-            
-            # Pembaruan posisi partikel berdasarkan kecepatan
-            self.particles[i] = self.apply_velocity(particle, self.velocities[i])
 
-            # Perbarui personal best (p_best)
-            if self.route_distance(self.particles[i]) < self.route_distance(self.p_best[i]):
-                self.p_best[i] = self.particles[i]
-
-        # Perbarui global best (g_best)
-        self.g_best = min(self.p_best, key=lambda p: self.route_distance(p))
+    def initialize_velocity(self):
+        # Inisialisasi kecepatan sebagai daftar pertukaran acak
+        return [(random.randint(0, len(self.waypoints) - 1), random.randint(0, len(self.waypoints) - 1)) for _ in range(len(self.waypoints))]
 
     def apply_velocity(self, particle, velocity):
         # Terapkan pertukaran posisi berdasarkan kecepatan
-        for swap in velocity:
-            i, j = swap, (swap + 1) % len(particle)
+        for i, j in velocity:
             particle[i], particle[j] = particle[j], particle[i]
         return particle
 
+    def update_particles(self):
+        for i, particle in enumerate(self.particles):
+            # Update kecepatan partikel
+            cognitive_component = [(random.randint(0, len(self.waypoints) - 1), random.randint(0, len(self.waypoints) - 1)) for _ in range(int(self.c1))]
+            social_component = [(random.randint(0, len(self.waypoints) - 1), random.randint(0, len(self.waypoints) - 1)) for _ in range(int(self.c2))]
+
+            # Gabungkan kecepatan dengan faktor inersia, kognitif, dan sosial
+            self.velocities[i] = self.apply_velocity(self.velocities[i], cognitive_component + social_component)
+
+            # Update posisi partikel
+            self.particles[i] = self.apply_velocity(particle, self.velocities[i])
+
+            # Update personal best (p_best)
+            if self.route_distance(self.particles[i]) < self.route_distance(self.p_best[i]):
+                self.p_best[i] = self.particles[i]
+
+        # Update global best (g_best)
+        self.g_best = min(self.p_best, key=lambda p: self.route_distance(p))
+
     def optimize(self):
         best_distance = float('inf')
+        best_route = None
         for iteration in range(self.num_iterations):
             self.update_particles()
             g_best_distance = self.route_distance(self.g_best)
@@ -80,6 +91,70 @@ class PSO_TSP:
                 best_distance = g_best_distance
                 best_route = self.g_best
 
-            print(f"Iterasi {iteration+1}/{self.num_iterations}, Jarak Terbaik: {best_distance:.2f} km")
+            print(f"Iteration {iteration+1}/{self.num_iterations}, Best Distance: {best_distance:.2f} km")
 
         return best_route, best_distance
+
+# # Kelas PSO untuk TSP
+# class PSO_TSP:
+#     def __init__(self, waypoints, start_point, end_point, num_particles, num_iterations, alpha, beta):
+#         self.waypoints = waypoints
+#         self.start_point = start_point
+#         self.end_point = end_point
+#         self.num_particles = num_particles
+#         self.num_iterations = num_iterations
+#         self.alpha = alpha  # Faktor inersia
+#         self.beta = beta    # Faktor pembelajaran
+
+#         self.distances = self.calculate_distances_matrix()
+#         self.particles = [random.sample(range(len(waypoints)), len(waypoints)) for _ in range(num_particles)]
+#         self.p_best = self.particles[:]
+#         self.g_best = min(self.particles, key=lambda p: self.route_distance(p))
+#         self.velocities = [random.sample(range(len(waypoints)), len(waypoints)) for _ in range(num_particles)]
+#     def calculate_distances_matrix(self):
+#         num_waypoints = len(self.waypoints)
+#         distances = np.zeros((num_waypoints, num_waypoints))
+#         for i in range(num_waypoints):
+#             for j in range(i + 1, num_waypoints):
+#                 distances[i][j] = distance_between_points(self.waypoints[i], self.waypoints[j])
+#                 distances[j][i] = distances[i][j]
+#         return distances
+
+#     def route_distance(self, route):
+#         waypoints_order = [self.waypoints[i] for i in route]
+#         return total_distance(waypoints_order, self.start_point, self.end_point)
+#     def update_particles(self):
+#         for i, particle in enumerate(self.particles):
+#             # Update kecepatan
+#             self.velocities[i] = random.sample(range(len(self.waypoints)), len(self.waypoints))
+            
+#             # Pembaruan posisi partikel berdasarkan kecepatan
+#             self.particles[i] = self.apply_velocity(particle, self.velocities[i])
+
+#             # Perbarui personal best (p_best)
+#             if self.route_distance(self.particles[i]) < self.route_distance(self.p_best[i]):
+#                 self.p_best[i] = self.particles[i]
+
+#         # Perbarui global best (g_best)
+#         self.g_best = min(self.p_best, key=lambda p: self.route_distance(p))
+
+#     def apply_velocity(self, particle, velocity):
+#         # Terapkan pertukaran posisi berdasarkan kecepatan
+#         for swap in velocity:
+#             i, j = swap, (swap + 1) % len(particle)
+#             particle[i], particle[j] = particle[j], particle[i]
+#         return particle
+
+#     def optimize(self):
+#         best_distance = float('inf')
+#         for iteration in range(self.num_iterations):
+#             self.update_particles()
+#             g_best_distance = self.route_distance(self.g_best)
+
+#             if g_best_distance < best_distance:
+#                 best_distance = g_best_distance
+#                 best_route = self.g_best
+
+#             print(f"Iterasi {iteration+1}/{self.num_iterations}, Jarak Terbaik: {best_distance:.2f} km")
+
+#         return best_route, best_distance
