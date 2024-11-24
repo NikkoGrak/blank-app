a62032738a2a32866b8d8b18600e10b1f25351d3
NikkoGrak <106216132+NikkoGrak@users.noreply.github.com>
2024-11-24 20:51:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -194,11 +194,14 @@ if uploaded_file is not None:
         # Membaca waypoint dari file Excel
         pso = PSO_TSP(waypoints_coordinates, start_point, end_point, num_particles, num_iterations, w, c1, c2)
         best_route_indices, best_distance = pso.optimize()
+
+        # Tambahkan 1 ke setiap indeks di best_route_indices untuk ditampilkan
+        best_route_indices_display = [i + 1 for i in best_route_indices]
         
         col3.write("**Particle Swarm Optimization Result:**")
         
 
-        col3.write(f"Optimal Route: {best_route_indices}")
+        col3.write(f"Optimal Route: {best_route_indices_display}")
         col3.write(f"Total Distance: {best_distance} km")
 
         end_time = time.time()
